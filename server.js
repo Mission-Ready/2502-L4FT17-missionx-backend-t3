786b3f827811cf323c59ea18393f40c3d3627274
Eugene Lee <eugenel@missionreadyhq.com>
2025-04-14 10:11:59 +1200

    nothing changed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 require("dotenv").config();
 const app = express();
 
+
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
