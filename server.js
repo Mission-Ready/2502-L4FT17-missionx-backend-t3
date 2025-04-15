64c2495f24ec6cc0bc4ddb38bb63362f273b3013
LuCinema <luciferknightingale@gmail.com>
2025-04-16 11:44:17 +1200

    Backend: update server changes on kerry

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mysql = require("mysql2");
 require("dotenv").config();
+const cors = require('cors');
+
 const app = express();
 
 const pool = mysql.createPool({
@@ -13,7 +15,9 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 //example
 app.get("/api/teacherdashboard/helprequest", (req, res) => {
   pool.query("SELECT * FROM teacher", (err, result) => {
@@ -36,10 +40,34 @@ app.get("/api/teacherdashboard/helprequest", (req, res) => {
 
 
 
-// Kerrys enpoints here
+// Kerrys endpoints here
+app.get("/api/studentProfileViewer/:studentId", (req, res) => {
+  const studentId = req.params.studentId;
 
+  pool.query('SELECT * FROM student WHERE student_id = ?', [studentId], (err, result) => {
+    if (err) {
+      console.error("Database query error", err);
+      return res.status(500).json({
+        status: "error",
+        message: "something went wrong with that query",
+      });
+    }
+    res.status(200).json({ status: "success", data: result });
+  });
+});
 
-
+app.get("/api/projectLibrary", (req, res) => {
+  pool.query('SELECT * FROM project', (err, result) => {
+    if (err) {
+      console.error("Database query error", err);
+      return res.status(500).json({
+        status: "error",
+        message: "Database query failed",
+      });
+    }
+    res.status(200).json({ status: "success", data: result });
+  });
+});
 
 
 // Solomene endpoint here
